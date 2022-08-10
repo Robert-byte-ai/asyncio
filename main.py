6434ceeed2b2c34fb5b6f669a00634e53581d181
Robert Saakyan <79167287+Robert-byte-ai@users.noreply.github.com>
2022-08-10 18:13:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ from typing import Optional, Any, Dict, Set, List
 WORKERS_COUNT = 10
 MAX_PARALLEL_AGG_REQUESTS_COUNT = 5
 
-ERROR_TIMEOUT_MSG = (
-    "Слишком долгое ожидание, вероятно программа повисла в бесконечном ожидании"
-)
 
 
 class PipelineContext:
